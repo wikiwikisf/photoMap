d3fe1acbed429c0d7bb815d37c2caeafa3ef9d0a
Vicki Grospe <vickichun@gmail.com>
2015-10-21 19:21:03 -0700

    M3

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,11 +7,43 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func cameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let vc = UIImagePickerController()
+            vc.delegate = self
+            vc.allowsEditing = true
+            vc.sourceType = UIImagePickerControllerSourceType.Camera
+            self.presentViewController(vc, animated: true, completion: nil)
+        } else {
+            let vc = UIImagePickerController()
+            vc.delegate = self
+            vc.allowsEditing = true
+            vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            self.presentViewController(vc, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+            MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
 
         // Do any additional setup after loading the view.
     }
